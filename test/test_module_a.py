e3240d5ad5716a12cf3984562dbb3e3e787d74e5
burstknight <burstknight@gmail.com>
2023-10-04 11:44:43 +0800

    test: Add labels into `test_module_a`
    - Target:
      The test module `test_module_a` need add labels inot each functions to
      classify each unit test.
    
    - Changed:
      Add labels into `test_module_a`.

--- test/test_module_a.py
+++ test/test_module_a.py
@@ -1,10 +1,12 @@
 from src.module_a import square, concat
 import pytest
 
+@pytest.mark.math
 def test_square():
     assert square(8) == 64
 # End of test_square
 
+@pytest.mark.string
 def test_concat():
     sA = "Hello! "
     sB = "Joe!"
@@ -12,6 +14,7 @@ def test_concat():
     assert concat(sA, sB) == "Hello! Joe!"
 # End of test_concat
 
+@pytest.mark.string
 def test_concat_failed():
     sA = 555
     sB = 777
