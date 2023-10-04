10f21a17820e1c8be1ea4374549c1b72011e4f91
burstknight <burstknight@gmail.com>
2023-10-04 11:29:51 +0800

    feat: Add func `concat()` into `module_a`
    - Target:
      This repo need implement a function to concat two strings as a string.
    
    - Changed:
      Add the function into `module_a`.

--- src/module_a.py
+++ src/module_a.py
@@ -14,3 +14,25 @@ def square(num: int) -> int:
     """
     return num**2
 # End of square
+
+def concat(s1: str, s2: str) -> str:
+    """
+    Description:
+    =======================================================
+    Concat two strings as a string.
+
+    Args:
+    =======================================================
+    - s1: Give the first string.
+    - s2: Give the second string.
+
+    Returns:
+    =======================================================
+    - rtype: str, Return the concated string.
+    """
+    if not (isinstance(s1, str) and isinstance(s2, str)):
+        raise TypeError("Paramenter type error!")
+    else:
+        return s1 + s2
+    # End of if-condition
+# End of concat
