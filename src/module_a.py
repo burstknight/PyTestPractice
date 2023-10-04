83f096b84a9e17581fc6a58dc3688c7010b8574d
burstknight <burstknight@gmail.com>
2023-10-04 11:20:43 +0800

    feat: Add `module_a`
    - Target:
      This repo need a module to implement square operation.
    
    - Changed:
      Add `module_a`.

---
+++ src/module_a.py
@@ -0,0 +1,16 @@
+def square(num: int) -> int:
+    """
+    Description:
+    =======================================================
+    Calculate square for the given number.
+
+    Args:
+    =======================================================
+    - num: Give an integer.
+
+    Returns:
+    =======================================================
+    - rtype: int, Return a squared number.
+    """
+    return num**2
+# End of square
