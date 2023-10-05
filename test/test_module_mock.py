1f2e096e5662ed2d01b58a0b70039300bfd6ac7f
burstknight <burstknight@gamil.com>
2023-10-05 11:46:35 +0800

    test: Update unit test for `module_mock`
    - Target:
      The unit test `test_module_mock` need update to use mock to replace
      some functions with another functions to skip long time tests.
    
    - Changed:
      Update the unit test `test_module_mock`.

--- test/test_module_mock.py
+++ test/test_module_mock.py
@@ -1,7 +1,8 @@
-from src.module_mock import getCurrentMoth
+from src.module_mock import getCurrentMoth, sleepTime
 from unittest.mock import patch
 from datetime import datetime
-from pytest import raises
+from pytest import raises, mark
+import time
 
 def test_getCurrentMonth():
     with patch("src.module_mock.datetime") as oMockDatetime:
@@ -21,3 +22,32 @@ def test_getCurrentMonth_fail():
     # End of with-block
 # ENd of test_getCurrentMonth_fail
 
+
+@mark.long_time_test
+def test_sleepTime():
+    # Use the wrong wat to perform a long time test.
+
+    iResponse = sleepTime(20)
+
+    assert iResponse == 20
+# End of test_sleepTime
+
+def test_sleepTime_mock():
+    # Use the first way to use `patch` to replace `time.sleepTime()` with `None` object.
+
+    with patch("src.module_mock.time.sleep"):
+        iResponse = sleepTime(20)
+
+        assert iResponse == 20
+    # End of with-block
+# End of test_sleepTime_mock
+
+def test_sleepTime_replace():
+    # Use `patch` to replace `time.sleep()` with the function `time.sleep(2)`
+
+    with patch("src.module_mock.time.sleep", new_callable=time.sleep(2)):
+        iResponse = sleepTime(20)
+
+        assert iResponse == 20
+    # End of with-block
+# End of test_sleepTime_replace
