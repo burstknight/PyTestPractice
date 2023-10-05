f10ca4fa982f71716944597fae998080d3e9f3f3
burstknight <burstknight@gamil.com>
2023-10-05 11:04:54 +0800

    test: Add test data with `pytest.fixture()`
    - Target:
      This repo need a module to define test data with `pytest.fixture()`.
    
    - Changed:
      - Add the test data module `fixture`.
      - Update the test module `test_module_b` to use the test data module
        `fixture`.

---
+++ test/fixture.py
@@ -0,0 +1,6 @@
+import pytest
+
+@pytest.fixture()
+def test_dict():
+    return {"a": 1, "b": 2}
+# End of test_dict
--- test/test_module_b.py
+++ test/test_module_b.py
@@ -1,25 +1,20 @@
 import pytest
 from src.module_b import updateValueByKey, checkKeyExists
+from test.fixture import test_dict # import test_dict
 
-def test_updateValueByKey():
-    dctTest = {"a": 1, "b": 2}
-
-    dctNew = updateValueByKey(dctTest, "b", 999 )
+def test_updateValueByKey(test_dict):
+    dctNew = updateValueByKey(test_dict, "b", 999 )
 
     assert dctNew["b"] == 999
 # End of test_updateValueByKey
 
-def test_updateValueByKey_error():
-    dctTest = {"a": 1, "b": 2}
-
+def test_updateValueByKey_error(test_dict):
     with pytest.raises(KeyError):
-        dctNew = updateValueByKey(dctRaw=dctTest, key="error_key", value=999)
+        dctNew = updateValueByKey(dctRaw=test_dict, key="error_key", value=999)
     # End of with-block
 # End of test_updateValueByKey_error
 
-def test_checkKeyExits():
-    dctTest = {"a": 1, "b": 2}
-
-    assert checkKeyExists(dctData=dctTest, key="a")
-    assert not checkKeyExists(dctData=dctTest, key="not existed")
+def test_checkKeyExits(test_dict):
+    assert checkKeyExists(dctData=test_dict, key="a")
+    assert not checkKeyExists(dctData=test_dict, key="not existed")
 
